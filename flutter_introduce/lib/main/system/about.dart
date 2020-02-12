9bc4f4c43f2506aa9ae38dfc4c63ad927f880686
ke_wang <>
2020-02-12 11:03:48 +0800

    关于

--- flutter_introduce/lib/main/system/about.dart
+++ flutter_introduce/lib/main/system/about.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:package_info/package_info.dart';
 
-class About extends StatelessWidget {
-  const About({Key key}) : super(key: key);
+class About extends StatefulWidget {
+  About({Key key}) : super(key: key);
+
+  @override
+  _AboutState createState() => _AboutState();
+}
+
+class _AboutState extends State<About> {
+  String version = "";
+
+  @override
+  void initState() { 
+    super.initState();
+    PackageInfo.fromPlatform().then((PackageInfo packageInfo){
+      setState(() {
+        version = packageInfo.version; 
+      });
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("关于我们"),
+        title: Text("关于APP"),
       ),
       body: Column(
         children: <Widget>[
@@ -17,7 +36,27 @@ class About extends StatelessWidget {
               top: ScreenUtil().setHeight(50),
             ),
             alignment: Alignment.center,
-            child: Icon(Icons.help, size:80,),
+            child: Icon(
+              Icons.help,
+              size: 80,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: ScreenUtil().setHeight(20),
+            ),
+            child: Text(
+              "NAME",
+              style: TextStyle(
+                fontSize: ScreenUtil().setSp(28),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: ScreenUtil().setHeight(20),
+            ),
+            child: Text("Version $version"),
           ),
           
         ],
